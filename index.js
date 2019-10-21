dd2772a5a85de42a3aa21d9466368413abb4a7f1
lopatakopaet <46110520+lopatakopaet@users.noreply.github.com>
2019-10-21 16:33:24 +0300

    add style css
    add style css

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let userRequestInput = document.querySelector('.search_input');
 let buttonElem = document.querySelector('.search_button');
+let booksSectionElem = document.querySelector('.books_section_wrap');
 
 // https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor
+
 
 class getBooks {
     constructor(url, request) {
@@ -24,15 +26,36 @@ class getBooks {
         let test = await fetch(`${urlApi}${url}${request}`);
         let data = await test.json();
         return data;
-        // this.testArr = data;
     }
 }
 
-// new getBooks('volumes','?q=Сапковски+inauthor&').getRequest();
-let a = new getBooks('volumes', '?q=Сапковски');
+/**
+ *
+ */
 
 buttonElem.addEventListener('click', (e) => {
     e.preventDefault();
+    booksSectionElem.innerHTML = `
+        <div class="loading-window">
+        <div class="car">
+        <div class="strike"></div>
+        <div class="strike strike2"></div>
+        <div class="strike strike3"></div>
+        <div class="strike strike4"></div>
+        <div class="strike strike5"></div>
+        <div class="car-detail spoiler"></div>
+        <div class="car-detail back"></div>
+        <div class="car-detail center"></div>
+        <div class="car-detail center1"></div>
+        <div class="car-detail front"></div>
+        <div class="car-detail wheel"></div>
+        <div class="car-detail wheel wheel2"></div>
+        </div>
+
+        <div class="text">
+            <span>Loading</span><span class = "dots">...</span>
+        </div>
+    `;
     let paramsString = 'q=flowers';
     // let paramsString = 'https://www.googleapis.com/books/v1/volumes?q=test';
     let searchParams = new URLSearchParams(paramsString);
@@ -43,23 +66,56 @@ buttonElem.addEventListener('click', (e) => {
         }
     }
     let newQuery = new getBooks('volumes?', `${searchParams.toString()}`);
-    newQuery.getRequest().then(console.log);
+    newQuery.getRequest().then(console.log);//////////////удалить
     newQuery.getRequest()
         .then((res) => {
-
+            let tempNode = document.createElement('div');
             res.items.map(elem => {
                 let node = document.createElement('div');
+                node.classList.add('book_block_wrap');
                 node.innerHTML = `
-                <img src="${elem.volumeInfo.imageLinks.smallThumbnail}" alt="">
-                
+                    <div class="img_wrap">
+                        <img src="${elem.volumeInfo.imageLinks.smallThumbnail}" alt="книга ${elem.volumeInfo.title}">
+                    </div>
+                    <div class="book_info_wrap">
+                        <p><span class="bold">Название книги: </span><span class="cursive">${elem.volumeInfo.title}</span></p>
+                        <p><span class="bold">Автор(ы):</span> ${elem.volumeInfo.authors ? elem.volumeInfo.authors : "Автор не найден"}</p>
+                        <p> <span class="bold">Опубликованно:</span> ${elem.volumeInfo.publishedDate ? elem.volumeInfo.publishedDate : 'Дата публикации не найдена'}</p>
+                         <p><span class="bold">Цена:</span> ${elem.saleInfo.saleability == "FOR_SALE" ? elem.saleInfo.listPrice.amount: 'FREE' }</p>
+                    </div>
+
             `;
-                test.appendChild(node);
-                if (elem.saleInfo.saleability == "FOR_SALE" ){
-                    console.log(elem.saleInfo.listPrice.amount)
-                }
-                else console.log('FREE')
+                tempNode.appendChild(node);
             })
-        });
+            booksSectionElem.innerHTML = tempNode.innerHTML;
+    //         return res.items.map(elem => {
+    //             let node = document.createElement('div');
+    //
+    //             node.classList.add('book_block_wrap');
+    //             node.innerHTML = `
+    //                 <div class="img_wrap">
+    //                     <img src="${elem.volumeInfo.imageLinks.smallThumbnail}" alt="книга по запросу">
+    //                 </div>
+    //                 <span>Название книги:${elem.volumeInfo.title}</span>
+    //                 <span>Автор(ы): ${elem.volumeInfo.authors ? elem.volumeInfo.authors : "Автор не найден"}</span>
+    //                 <span>Цена: ${elem.saleInfo.saleability == "FOR_SALE" ? elem.saleInfo.listPrice.amount: 'FREE' }</span>
+    //                 <span>Опубликованно: ${elem.volumeInfo.publishedDate ? elem.volumeInfo.publishedDate : 'Дата публикации не найдена'}</span>
+    //
+    //         `;
+    //
+    //         })
+    //
+    //
+    //
+        })
+    // .then(res => {
+    //     booksSectionElem.innerHTML = '';
+    //     booksSectionElem.appendChild(res);
+    //     if (elem.saleInfo.saleability == "FOR_SALE") {
+    //         console.log(elem.saleInfo.listPrice.amount)
+    //     } else console.log('FREE')
+    //     console.log(res)
+    // })
 })
 /**
  * ${elem.volumeInfo.publishedDate}  опубликовано
